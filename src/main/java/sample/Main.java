cdf544858435b8bb419b7a44a1b5cbc1e3714a3b
rapahel kim <sudokky.dev@gmail.com>
2014-08-14 17:00:51 +0900

    Apply to use logback for logging
    [launch time]
    real    0m0.216s
    user    0m0.243s
    sys     0m0.022s

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,9 +1,16 @@
 package sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello world, Simple java app with maven");
+		if (logger.isInfoEnabled()) {
+			logger.info("Hello world, Simple java app with maven");
+		}
 	}
 
 }
